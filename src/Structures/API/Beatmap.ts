a2ab95302f5f7e50955696e89eca0c752b268d3f
MoeLava <me@lava.moe>
2020-04-12 15:39:12 +0700

    Add typings for v2 Beatmap and Beatmapset structures

--- src/Structures/API/Beatmap.ts
+++ src/Structures/API/Beatmap.ts
@@ -13,24 +13,37 @@ export interface Beatmap {
     ar: number;
     beatmapset?: BeatmapSet;
     beatmapset_id: number;
+    bpm: number;
+    /** Unclear */
+    convert: {} | null;
     count_circles: number;
     count_sliders: number;
+    count_spinners: number;
+    count_total: number;
     cs: number;
+    deleted_at: string | null;
     difficulty_rating: number;
     drain: number;
-    failtimes: Metric;
-    max_combo: number;
-    mode_int: GameMode;
+    failtimes?: Metric;
+    hit_length: number;
+    is_scoreable: boolean;
+    last_updated: string;
+    max_combo?: number;
+    mode: GameMode;
+    mode_int: number;
     passcount: number;
     playcount: number;
+    ranked: number;
     status: BeatmapSetOnlineStatus;
     total_length: number;
+    url: string;
     version: number;
 }
 
 /**
  * Beatmap metrics based on accumulated online data from community plays.
- * Reference: {@link https://github.com/ppy/osu/blob/master/osu.Game/Beatmaps/BeatmapMetrics.cs}
+ * - References:
+ *   - {@link https://github.com/ppy/osu/blob/master/osu.Game/Beatmaps/BeatmapMetrics.cs}
  */
 interface Metric {
     /** Points of retry on a relative time scale (usually 0..100). */
--- src/Structures/API/BeatmapSet.ts
+++ src/Structures/API/BeatmapSet.ts
@@ -5,41 +5,61 @@ import { BeatmapSetOnlineStatus } from "../../Enums";
  * **BeatmapSet**
  * - **Undocumented**
  * - References:
+ *   - Response sample from `/api/v2/users/{user}/beatmapsets/favourite`
  *   - {@link https://github.com/ppy/osu/blob/master/osu.Game/Online/API/Requests/Responses/APIBeatmapSet.cs}
  */
 export interface BeatmapSet {
     id: number;
+    artist: string;
     availability: Availability;
     beatmaps?: Beatmap[];
     bpm: number;
     covers: Covers;
-    favourite_count: string;
-    genre: Genre;
-    has_favourited: string;
-    language: Language;
+    creator: string;
+    discussion_enabled: boolean;
+    discussion_locked: false;
+    favourite_count: number;
+    genre?: Genre;
+    has_favourited: boolean;
+    hype: Requirement;
+    is_scoreable: boolean;
+    language?: Language;
     last_updated: string;
-    play_count: string;
+    legacy_thread_url: string;
+    nominations: Requirement;
+    play_count: number;
     preview_url: string;
+    ranked: number;
     ranked_date: string | null;
-    ratings: number[];
+    ratings?: number[];
+    source: string;
     status: BeatmapSetOnlineStatus;
     storyboard: boolean;
     submitted_date: string;
-    video: boolean;
+    tags: string;
+    title: string;
     user_id: number;
+    video: boolean;
 }
 
 /**
- * Reference: {@link https://github.com/ppy/osu/blob/133a3d3e0f15ebb4ef48c302dac04b6346316c24/osu.Game/Beatmaps/BeatmapSetOnlineInfo.cs#L102}
+ * - References:
+ *   - {@link https://github.com/ppy/osu/blob/133a3d3e0f15ebb4ef48c302dac04b6346316c24/osu.Game/Beatmaps/BeatmapSetOnlineInfo.cs#L102}
  */
 interface Covers {
+    "cover": string;
     "cover@2x": string;
+    "card": string;
     "card@2x": string;
+    "list": string;
     "list@2x": string;
+    "slimcover": string;
+    "slimcover@2x": string;
 }
 
 /**
- * Reference: {@link https://github.com/ppy/osu/blob/133a3d3e0f15ebb4ef48c302dac04b6346316c24/osu.Game/Beatmaps/BeatmapSetOnlineInfo.cs#L120}
+ * - References:
+ *   - {@link https://github.com/ppy/osu/blob/133a3d3e0f15ebb4ef48c302dac04b6346316c24/osu.Game/Beatmaps/BeatmapSetOnlineInfo.cs#L120}
  */
 interface Availability {
     download_disabled: boolean;
@@ -49,7 +69,8 @@ interface Availability {
 /**
  * **Unclear**
  *
- * Reference: {@link https://github.com/ppy/osu/blob/133a3d3e0f15ebb4ef48c302dac04b6346316c24/osu.Game/Beatmaps/BeatmapSetOnlineInfo.cs#L90}
+ * - References:
+ *   - {@link https://github.com/ppy/osu/blob/133a3d3e0f15ebb4ef48c302dac04b6346316c24/osu.Game/Beatmaps/BeatmapSetOnlineInfo.cs#L90}
  */
 interface Genre {
     id: string;
@@ -59,9 +80,15 @@ interface Genre {
 /**
  * **Unclear**
  *
- * Reference: {@link https://github.com/ppy/osu/blob/133a3d3e0f15ebb4ef48c302dac04b6346316c24/osu.Game/Beatmaps/BeatmapSetOnlineInfo.cs#L96}
+ * - References:
+ *   - {@link https://github.com/ppy/osu/blob/133a3d3e0f15ebb4ef48c302dac04b6346316c24/osu.Game/Beatmaps/BeatmapSetOnlineInfo.cs#L96}
  */
 interface Language {
     id: string;
     name: string;
+}
+
+interface Requirement {
+    current: number;
+    required: number;
 }
