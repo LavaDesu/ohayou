3c52f5f6a0853fe48d5066fe727b2a2aee784f00
LavaDesu <me@lava.moe>
2021-01-02 23:57:25 +0700

    Update endpoints
    - Add a TODO comment for every unimplemented endpoint
    - Add additional exposed endpoints
    - Remove endpoints that aren't exposed from the API

--- src/Endpoints.ts
+++ src/Endpoints.ts
@@ -5,30 +5,40 @@ export enum Endpoints {
     API_PREFIX = "/api/v2",
     OAUTH_PREFIX = "/oauth",
 
-    COMMENT = "/comments",
-    COMMENT_SINGLE = "/comments/{comment}",
-
-    RANKING_GET = "/rankings/{mode}/{type}",
-    RANKING_GET_SPOTLIGHT = "/spotlights",
-
     TOKEN = "/token",
 
-    BEATMAP_SINGLE = "/beatmaps/{beatmap}",
+    BEATMAP_SINGLE = "/beatmaps/{beatmap}", // TODO: unimplemented
     BEATMAP_LOOKUP = "/beatmaps/lookup",
     BEATMAP_SCORES = "/beatmaps/{id}/scores",
 
-    BEATMAPSET_SINGLE = "/beatmapsets/{beatmapset}",
-    BEATMAPSET_LOOKUP = "/beatmapsets/lookup",
-    BEATMAPSET_SEARCH = "/beatmapsets/search/{filters}",
+    BEATMAPSET_SINGLE = "/beatmapsets/{beatmapset}", // TODO: unimplemented
+    BEATMAPSET_EVENTS = "/beatmapsets/events", // TODO: unimplemented
+    BEATMAPSET_LOOKUP = "/beatmapsets/lookup", // TODO: unimplemented
+    BEATMAPSET_SEARCH = "/beatmapsets/search/{filters}", // TODO: unimplemented
 
-    CHANGELOG = "/changelog",
-    CHANGELOG_BUILD = "/changelog/{stream}/{build}",
-    CHANGELOG_SINGLE = "/changelog/{changelog}",
+    CHANGELOG = "/changelog", // TODO: unimplemented
+    CHANGELOG_BUILD = "/changelog/{stream}/{build}", // TODO: unimplemented
+    CHANGELOG_SINGLE = "/changelog/{changelog}", // TODO: unimplemented
 
     FRIEND = "/friends",
     ME = "/me/{mode}",
 
-    SEASONAL_BACKGROUNDS = "/seasonal-backgrounds",
+    NEWS = "/news", // TODO: unimplemented
+    NEWS_SINGLE = "/news/{news}", // TODO: unimplemented
+
+    RANKINGS = "/rankings/{mode}/{type}", // TODO: unimplemented
+
+    ROOMS = "/rooms/{mode?}", // TODO: unimplemented
+    ROOM_SINGLE = "/rooms/{room}", // TODO: unimplemented
+    ROOM_LEADERBOARD = "/rooms/{room}/leaderboard", // TODO: unimplemented
+    ROOM_SCORES = "/rooms/{room}/playlist/{playlist}/scores", // TODO: unimplemented
+
+    SCORE_SINGLE = "/scores/{mode}/{score}", // TODO: unimplemented
+    SCORE_DOWNLOAD = "/scores/{mode}/{score}/download", // TODO: unimplemented
+
+    SEASONAL_BACKGROUNDS = "/seasonal-backgrounds", // TODO: unimplemented
+
+    SPOTLIGHTS = "/spotlights", // TODO: unimplemented
 
     USER_SINGLE = "/users/{user}/{mode}",
     USER_BEATMAPSETS = "/users/{user}/beatmapsets/{type}",
